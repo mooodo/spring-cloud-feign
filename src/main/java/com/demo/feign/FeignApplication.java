279fb565c55a0769bca9d8a63b64d4ed20e242ed
Mooodo <fangang2004@126.com>
2021-02-13 05:17:59 +0800

    change the configuration

--- src/main/java/com/demo/feign/FeignApplication.java
+++ src/main/java/com/demo/feign/FeignApplication.java
@@ -6,7 +6,6 @@ package com.demo.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -15,7 +14,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrix
 public class FeignApplication {
 
 	/**
